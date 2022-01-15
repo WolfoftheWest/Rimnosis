3dcd1500f151c51f295ea81ba038c7eedfbed665
flock-of-birds <97801845+flock-of-birds@users.noreply.github.com>
2022-01-15 19:50:08 +0000

    Fixed an error(?) in Rimnosis1.cs just so omnisharp would STFU. The omnisharp.json trick wasn't working :v

--- Source/Rimnosis/Rimnosis/Rimnosis1.cs
+++ Source/Rimnosis/Rimnosis/Rimnosis1.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Verse;
 
 namespace WhiteNoiseGrenade
 {
-    public class Proj_GrenadeWN : Grenade
+    public class Proj_GrenadeWN : Projectile_Explosive
     {
         #region Properties
         //
